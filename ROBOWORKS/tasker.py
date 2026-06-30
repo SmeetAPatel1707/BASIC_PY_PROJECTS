bd26fea24ff329e3b2bc9188843472c8c38ac0c0
Smeet Patel <smeetp4636@gmail.com>
2026-06-30 22:19:06 +1000

    Add initial project structure and data files for ROBOWORKS

---
+++ ROBOWORKS/tasker.py
@@ -0,0 +1,16 @@
+'''
+This file should contain all task/schedule processing logic after data is loaded.⁠
+⁠​
+Put these functions here
+is_task_executable(...)
+check_schedule(...)
+Good helper functions for this file
+find_robot_by_id(...)
+find_package_by_id(...)
+find_destination_by_id(...)
+find_task_by_id(...)
+distance_from_matrix(...)
+can_robot_do_task(...)
+battery_cost_for_trip(...)
+
+'''
